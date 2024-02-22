0ef41976d8de939e2987ff128fd49be400af7b38
ChrisOrdi <chris.vandalen@ordina.nl>
2024-02-22 16:29:17 +0100

    Initial commit

---
+++ src/main/java/com/avans/cloudlocker/cloudlocker/CloudLockerApplication.java
@@ -0,0 +1,13 @@
+package com.avans.cloudlocker.cloudlocker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CloudLockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CloudLockerApplication.class, args);
+	}
+
+}
