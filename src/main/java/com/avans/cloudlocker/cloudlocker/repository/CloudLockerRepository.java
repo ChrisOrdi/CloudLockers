4713484733621ff0ce5c6601e69cbb4eef32eaa7
ChrisOrdi <chris.vandalen@ordina.nl>
2024-02-22 19:01:43 +0100

    voor get mappings toegevoegd, ook een postmapping voor Client toe te voegen

--- src/main/java/com/avans/cloudlocker/cloudlocker/repository/CloudLockerRepository.java
+++ src/main/java/com/avans/cloudlocker/cloudlocker/repository/CloudLockerRepository.java
@@ -1,6 +1,7 @@
 package com.avans.cloudlocker.cloudlocker.repository;
 
+import com.avans.cloudlocker.cloudlocker.model.CloudLocker;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface CloudLockerRepository extends JpaRepository<> {
+public interface CloudLockerRepository extends JpaRepository<CloudLocker, Long> {
 }
