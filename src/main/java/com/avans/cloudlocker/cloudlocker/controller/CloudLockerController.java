3ac253735ca04125fcda5c2650494d4ab3691b59
ChrisOrdi <chris.vandalen@ordina.nl>
2024-02-22 16:51:48 +0100

    init commit voor get mappings

--- src/main/java/com/avans/cloudlocker/cloudlocker/controller/CloudLockerController.java
+++ src/main/java/com/avans/cloudlocker/cloudlocker/controller/CloudLockerController.java
@@ -1,12 +1,24 @@
 package com.avans.cloudlocker.cloudlocker.controller;
 
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class CloudLockerController {
 
+    private int connection = 1;
+
     // Get Mappings
 
+    @GetMapping("/requestClientState")
+    public void getClientState() {
+        if (connection > 1) {
+            System.out.println("Client State werkt niet");
+        }
+        else {
+            System.out.println("Client State werkt");
+        }
+    }
 
     // Post Mappings
 
